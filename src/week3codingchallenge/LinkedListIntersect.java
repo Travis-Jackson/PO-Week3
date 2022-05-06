3b45be82fe4f4c6e87df7242e1f2c076ccf7b7bc
Travis-Jackson <98404483+Travis-Jackson@users.noreply.github.com>
2022-05-05 22:14:06 -0400

    week3 PO

---
+++ src/week3codingchallenge/LinkedListIntersect.java
@@ -0,0 +1,44 @@
+package week3codingchallenge;
+
+public class LinkedListIntersect {
+	
+	//list node definition
+	public class ListNode{
+		int val;
+		ListNode next;
+		ListNode(int x){
+			val = x;
+			next = null;
+		}
+	}
+	
+	public ListNode getIntersection(ListNode headA, ListNode headB ) {
+		
+		//check if either head is null, no intersection
+		if(headA == null || headB == null) {
+			return null;
+		}
+		
+		//create a pointer for each list
+		ListNode aPointer = headA;
+		ListNode bPointer = headB;
+		
+		//loop until the pointers meet
+		while(aPointer != bPointer) {
+			if(aPointer == null) {
+				aPointer = headB;
+			}else {
+				aPointer = aPointer.next;
+			}
+			
+			if(bPointer == null) {
+				bPointer = headA;
+			}else {
+				bPointer = bPointer.next;
+			}
+		}
+		
+		//return either pointer when they are equal
+		return aPointer;
+	}
+}
